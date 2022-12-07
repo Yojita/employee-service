7dcd319f8560ffebe09db9abd8dbab875e9dcac0
Tushar Girase <86668254+yojita@users.noreply.github.com>
2022-12-07 20:45:39 +0530

    Introduce repository
    Introduce repository using JPA annotations.

---
+++ src/main/java/com/abcl/emp/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.abcl.emp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.abcl.emp.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
