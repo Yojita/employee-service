b6e381e10ef23c2e17461a098596d10e3cad11e2
Tushar Girase <86668254+yojita@users.noreply.github.com>
2022-12-07 20:41:21 +0530

    Introduce entity
    Introduced entity using JPA annotations.

---
+++ src/main/java/com/abcl/emp/entity/Employee.java
@@ -0,0 +1,76 @@
+package com.abcl.emp.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long empId;
+	private String firstaName;
+	private String lastName;
+	private String empCode;
+	private Long deptId;
+	private LocalDate birthDate;
+
+	public Long getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+
+	public String getFirstaName() {
+		return firstaName;
+	}
+
+	public void setFirstaName(String firstaName) {
+		this.firstaName = firstaName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmpCode() {
+		return empCode;
+	}
+
+	public void setEmpCode(String empCode) {
+		this.empCode = empCode;
+	}
+
+	public Long getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(Long deptId) {
+		this.deptId = deptId;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", firstaName=" + firstaName + ", lastName=" + lastName + ", empCode="
+				+ empCode + ", deptId=" + deptId + ", birthDate=" + birthDate + "]";
+	}
+
+}
