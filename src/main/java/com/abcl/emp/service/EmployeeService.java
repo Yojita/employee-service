856c0296c84d98cf6f8a69f7d622e0df4e6a1168
Tushar Girase <86668254+yojita@users.noreply.github.com>
2022-12-09 13:25:43 +0530

    Introduce EmployeeService interface

---
+++ src/main/java/com/abcl/emp/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.abcl.emp.service;
+
+import java.util.List;
+
+import com.abcl.emp.dto.EmployeeDTO;
+
+public interface EmployeeService {
+
+	public List<EmployeeDTO> getAllEmployees();
+}
