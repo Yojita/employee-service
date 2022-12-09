6c4aef99d5606c096f50848ece4b6648be1f6d55
Tushar Girase <86668254+yojita@users.noreply.github.com>
2022-12-09 15:39:25 +0530

    Introduce getDtoFromEntity method
    Introduced getDtoFromEntity method to convert entity to dto.

--- src/main/java/com/abcl/emp/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/abcl/emp/service/impl/EmployeeServiceImpl.java
@@ -3,17 +3,37 @@ package com.abcl.emp.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.abcl.emp.dto.EmployeeDTO;
+import com.abcl.emp.entity.Employee;
+import com.abcl.emp.repository.EmployeeRepository;
 import com.abcl.emp.service.EmployeeService;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
 	@Override
 	public List<EmployeeDTO> getAllEmployees() {
-		return new ArrayList<>();
+		List<Employee> allEmployee = employeeRepository.findAll();
+		List<EmployeeDTO> dtos = new ArrayList<>();
+		for (Employee employee : allEmployee) {
+			dtos.add(getDtoFromEntity(employee));
+		}
+		return dtos;
+	}
+
+	private EmployeeDTO getDtoFromEntity(Employee employee) {
+		EmployeeDTO dto = new EmployeeDTO();
+		dto.setFirstName(employee.getFirstaName());
+		dto.setLastName(employee.getLastName());
+		dto.setEmpCode(employee.getEmpCode());
+		dto.setBirthDate(employee.getBirthDate());
+		return dto;
 	}
 
 }
