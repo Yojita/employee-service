4cccecd03b8627065b47d947db4fd14ef7101b6d
Tushar Girase <86668254+yojita@users.noreply.github.com>
2023-01-23 15:08:41 +0530

    Create interface EmployeeService

--- src/main/java/com/abcl/emp/service/EmployeeService.java
+++ src/main/java/com/abcl/emp/service/EmployeeService.java
@@ -3,10 +3,14 @@ package com.abcl.emp.service;
 import java.util.List;
 
 import com.abcl.emp.dto.EmployeeDTO;
+import com.abcl.emp.dto.EmployeeSignUpDTO;
+import com.abcl.emp.exception.EmployeeServiceException;
 
 public interface EmployeeService {
 
 	public List<EmployeeDTO> getAllEmployees();
 
-	public EmployeeDTO getEmpByEmpCode(String empCode);
+	public EmployeeDTO getEmpByEmpCode(String empCode) throws EmployeeServiceException;
+
+	public EmployeeDTO createEmployeeRecord(EmployeeSignUpDTO dto) throws EmployeeServiceException;
 }
