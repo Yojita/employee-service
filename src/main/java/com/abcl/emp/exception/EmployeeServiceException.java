52eb725e2541c1125ffb0f3c8258cff61e7f9c22
Tushar Girase <86668254+yojita@users.noreply.github.com>
2023-01-23 15:32:12 +0530

    Create EmployeeServiceException

---
+++ src/main/java/com/abcl/emp/exception/EmployeeServiceException.java
@@ -0,0 +1,15 @@
+package com.abcl.emp.exception;
+
+public class EmployeeServiceException extends Exception {
+
+	private static final long serialVersionUID = 8556119168498996166L;
+
+	public EmployeeServiceException() {
+		super();
+	}
+
+	public EmployeeServiceException(String message) {
+		super(message);
+	}
+
+}
