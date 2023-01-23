d8215c9741483efb591429fb748c56522a8445bd
Tushar Girase <86668254+yojita@users.noreply.github.com>
2023-01-23 15:24:12 +0530

    UpdateEmployeeServiceApplication

--- src/main/java/com/abcl/emp/EmployeeServiceApplication.java
+++ src/main/java/com/abcl/emp/EmployeeServiceApplication.java
@@ -2,8 +2,12 @@ package com.abcl.emp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients(basePackages = "com.abcl.emp.feignClient")
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
