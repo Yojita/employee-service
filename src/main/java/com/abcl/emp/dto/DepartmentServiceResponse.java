ef36c79b172df6cd08e8ecc53f0cf2546517a34b
Tushar Girase <86668254+yojita@users.noreply.github.com>
2023-01-23 15:23:18 +0530

    Create DepartmentServiceResponse

---
+++ src/main/java/com/abcl/emp/dto/DepartmentServiceResponse.java
@@ -0,0 +1,14 @@
+package com.abcl.emp.dto;
+
+public class DepartmentServiceResponse {
+
+	private String deptCode;
+
+	public String getDeptCode() {
+		return deptCode;
+	}
+
+	public void setDeptCode(String deptCode) {
+		this.deptCode = deptCode;
+	}
+}
