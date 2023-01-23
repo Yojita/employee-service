13716bff7c65ad7a1af3195942668759ec82b93c
Tushar Girase <86668254+yojita@users.noreply.github.com>
2023-01-23 15:17:34 +0530

    Create APIResponse

---
+++ src/main/java/com/abcl/emp/dto/APIResponse.java
@@ -0,0 +1,24 @@
+package com.abcl.emp.dto;
+
+public class APIResponse {
+
+	private Integer errorCode;
+	private String errorMessage;
+
+	public Integer getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(Integer errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+}
